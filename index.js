6cf712c2097837ec65e8243363f27491548b30da
Élise <atomheartother@gmail.com>
2018-04-14 20:24:27 +0100

    Using twitter's stream API instead

--- index.js
+++ index.js
@@ -15,70 +15,159 @@ var tClient = new Twitter({
 
 const dClient = new Discord.Client();
 
-var channels = {};
+// This is the stream variable, which handles receiving the twitter feed
+var stream = null;
 
-function addGet(channel, screenName, delay) {
-    var interval = setInterval(getLatestPic, (delay * 1000 ) * 60, channel, screenName);
-    console.log(channel.name);
-    console.log(interval);
-    if (!channels.hasOwnProperty(channel.id))
-        channels[channel.id] = {};
+// Users:
+// Dict of TwitterUser, using userId as key
+// TwitterUser:
+// channels: Array of Gets
+// Get:
+// channel: channel object
+var users = {};
 
-    channels[channel.id][screenName] = {
-        'delay' : delay,
-        'lastImg' : "",
-        'intervalId' : interval
-    };
+// Register the stream with twitter
+function createStream() {
+    if (stream != null)
+        stream.destroy();
+    stream = null;
+
+    userIds = [];
+    // Get all the user IDs
+    for (var id in users) {
+        if (!users.hasOwnProperty(id)) continue;
+
+        userIds.push(id);
+    }
+    // If there are none, we can just leave stream at null
+    if (userIds.length < 1)
+        return;
+
+    // Else, register the stream using our userIds
+    stream = tClient.stream('statuses/filter', {follow: userIds.toString()});
+
+    stream.on('data', function(tweet, err) {
+        if ((tweet.hasOwnProperty('in_reply_to_user_id')
+            && tweet.in_reply_to_user_id !== null) ||
+            tweet.hasOwnProperty('retweeted_status'))
+            // This is a reply or a retweet, ignore it
+            return;
+        if (!users.hasOwnProperty(tweet.user.id_str)) {
+            // Somehow we got a tweet from someone we don't follow anymore.
+            console.error("We got a tweet from someone we don't follow:");
+            console.error(tweet);
+            return;
+        }
+        for (var get of users[tweet.user.id_str].channels) {
+            postTweet(get.channel, tweet);
+        }
+    });
+
+    stream.on('error', function(err) {
+        console.error("Error getting a stream");
+        console.error(err);
+    });
 }
 
-function saveChannels() {
-    // Create a copy of the channels object, remove all timeouts from it
-    var channelsCopy = Object.assign({}, channels);
-    for (var id in channelsCopy) { // Iterate over channels
-        if (!channelsCopy.hasOwnProperty(id)) continue;
+function addGet(channel, userId) {
+    if (!users.hasOwnProperty(userId)) {
+        // Create the user object
+        users[userId] = {channels : []};
+    }
 
-        var channel = channelsCopy[id];
-        for (var name in channel) { // Iterate over gets in channels
-            if (!channel.hasOwnProperty(name)) continue;
-            var get = channel[name];
-            delete get.intervalId;
+    for (var get of users[userId].channels) {
+        // Get is already in there for this channel
+        if (get.channel.id == channel.id)
+            return;
+    }
+    users[userId].channels.push({
+        "channel" : channel,
+    });
+}
+
+// We save users as:
+// {
+//    "userId" : {channels: [channelId1, channelId2]}
+// }
+function saveUsers() {
+    // Create a copy of the channels object, remove all timeouts from it
+    console.log("Saving users...");
+    console.log(users);
+    var usersCopy = {};
+    for (var userId in users) { // Iterate over twitter users
+        if (!users.hasOwnProperty(userId)) continue;
+
+        usersCopy[userId] = {channels:[]};
+        for (var get of users[userId].channels) {
+            usersCopy[userId].channels.push(get.channel.id);
         }
     }
-    var json = JSON.stringify(channelsCopy);
+    console.log("Saved copy:");
+    console.log(usersCopy);
+    var json = JSON.stringify(usersCopy);
     fs.writeFile(config.getFile, json, 'utf8', function(err) {
         if (err !== null) {
-            console.error("Error saving channels object:");
+            console.error("Error saving users object:");
             console.error(err);
         }
     });
+}
+
+function loadUsers() {
+    fs.stat(config.getFile, function(err, stat) {
+        if (err == null) {
+            console.log("Found a get file at " + config.getFile);
+            fs.readFile(config.getFile, 'utf8', function(err, data) {
+                if (err) {
+                    console.error("There was a problem reading the config file");
+                    return;
+                }
+                // Restore the channels object from saved file
+                var usersCopy = JSON.parse(data);
+                for (var userId in usersCopy) { // Iterate over users
+                    if (!usersCopy.hasOwnProperty(userId)) continue;
+
+                    var channels = usersCopy[userId].channels;
+                    for (var channelId of channels) { // Iterate over gets in channels
+                        var channel = dClient.channels.get(channelId);
+                        addGet(channel, userId);
+                    }
+                }
+                // All users have been registered, we can request the stream from Twitter
+                createStream();
+            });
+        }
+    });
+}
+
+function postTweet(channel, tweet) {
+    var embed = null;
+    if (!(tweet.hasOwnProperty('extended_entities') &&
+          tweet.extended_entities.hasOwnProperty('media') &&
+          tweet.extended_entities.media.length > 0))
+    {
+        embed = new Discord.RichEmbed()
+            .setAuthor(tweet.user.name, tweet.user.profile_image_url_https)
+            .setColor(0x69B2D6)
+            .setDescription(tweet.text)
+    }
+    else
+    {
+        var imgurl = tweet.extended_entities.media[0].media_url_https;
+        embed = new Discord.RichEmbed()
+            .setAuthor(tweet.user.name, tweet.user.profile_image_url_https)
+            .setColor(0xD667CF)
+            .setFooter(tweet.text)
+            .setImage(tweet.extended_entities.media[0].media_url_https);
+    }
+    channel.send({embed});
 }
 
 function getLatestPic(channel, screenName) {
     tClient.get('statuses/user_timeline', {screen_name:screenName})
         .then(function(tweets){
             var tweet = tweets[0];
-            if (!(tweet.hasOwnProperty('extended_entities') &&
-                  tweet.extended_entities.hasOwnProperty('media') &&
-                  tweet.extended_entities.media.length > 0))
-                return  channel.send("Sorry, the latest tweet from " + tweet.user.name + " doesn't have a picture! Here's what it says:\n`" + tweet.text + "`");
-            var imgurl = tweet.extended_entities.media[0].media_url_https;
-            if (channels.hasOwnProperty(channel.id) &&
-                channels[channel.id].hasOwnProperty(screenName))
-            {
-                if (channels[channel.id][screenName].lastImg === imgurl)
-                {
-                    // This is a recurrent call. If the previous image was the same as this one, no need to post
-                    console.log("Tried posting same image from " + screenName + " twice, ignored");
-                    return;
-                }
-                channels[channel.id][screenName].lastImg = imgurl;
-            }
-            const embed = new Discord.RichEmbed()
-                  .setAuthor(tweet.user.name, tweet.user.profile_image_url_https)
-                  .setColor(0xD667CF)
-                  .setFooter(tweet.text)
-                  .setImage(tweet.extended_entities.media[0].media_url_https);
-            channel.send({embed});
+            postTweet(channel, tweet);
         })
         .catch(function(error){
             channel.send("Something went wrong fetching this user's last tweet, sorry! :c");
@@ -107,7 +196,7 @@ dClient.on('message', (message) => {
               .setDescription("This is all the things I can currently do:")
               .setFooter("Issues, suggestions? My creator is Tom'#4242")
               .addField(config.prefix + "pic", "This command will get the latest tweet from the given user and post its picture.\nUsage: `" + config.prefix + "pic <twitter screen name>`")
-              .addField(config.prefix + "startget", "This command will periodically fetch a picture from a twitter user's last tweet\nUsage: `" + config.prefix + "startget <twitter screen name> <time in minutes >= 15>`")
+              .addField(config.prefix + "startget", "This command will periodically fetch a picture from a twitter user's last tweet\nUsage: `" + config.prefix + "startget <twitter screen name>`")
               .addField(config.prefix + "stopget", "This command will stop automatically fetching pictures from a twitter user\nUsage: `" + config.prefix + "stopget <twitter screen name>`")
               .addField(config.prefix + "list", "Will print out a list of the twitter users you're currently getting pictures from.")
 
@@ -123,23 +212,20 @@ dClient.on('message', (message) => {
 
     if (command === "startget")
     {
-        if (args.length < 2)
-            return message.channel.send("This command will periodically fetch a picture from a twitter user's last tweet\nUsage: `" + config.prefix + "startget <twitter screen name> <time in minutes >= 15>`");
+        if (args.length < 1)
+            return message.channel.send("This command will periodically fetch a picture from a twitter user's last tweet\nUsage: `" + config.prefix + "startget <twitter screen name>`");
         var screenName = args[0];
-        var delay = parseInt(args[1]);
-        if (isNaN(delay))
-            return message.channel.send("I'm sorry, I don't think " + args[1] + " is a number!");
-        if (delay < 15)
-        {
-            if (!message.author.id === config.ownerId)
-                return message.channel.send("Sorry, the delay needs to be 15 minutes or over! Bots aren't allowed to go that fast :C");
-            else
-                message.channel.send("Overriding 15 minutes limit as " + message.author.username + " is my benevolent master");
-        }
         message.channel.send("I'm starting to get pictures from " + screenName + ", remember you can stop me at any time with `" + config.prefix + "stopget " + screenName + "` !");
-        getLatestPic(message.channel, screenName);
-        addGet(message.channel, screenName, delay);
-        saveChannels();
+        // getLatestPic(message.channel, screenName);
+        tClient.get('users/lookup', {'screen_name' : screenName})
+            .then(function(data) {
+                var userId = data[0].id_str;
+                addGet(message.channel, userId);
+                saveUsers();
+            })
+            .catch(function(error) {
+                return message.channel.send("I can't find a user by the name of " + screenName);
+            });
     }
 
     if (command === "stopget")
@@ -147,76 +233,47 @@ dClient.on('message', (message) => {
         if (args.length < 1)
             return message.channel.send("This command will stop automatically fetching pictures from a twitter user\nUsage: `" + config.prefix + "stopget <twitter screen name>`");
         var screenName = args[0];
-        if (!channels.hasOwnProperty(message.channel.id))
-            return message.channel.send("I'm afraid you don't have any oingoing `get`s! Use " + config.prefix + "startget to do that");
-        if (!(channels[message.channel.id].hasOwnProperty(screenName)))
-            return message.channel.send("This user isn't being `get`ted right now. Use " + config.prefix + "list to see what I'm currently doing!");
-        var intervalId = channels[message.channel.id][screenName].intervalId;
-        console.log(intervalId);
-        clearInterval(intervalId);
-        delete channels[message.channel.id][screenName];
-        if (Object.keys(channels[message.channel.id]).length === 0 &&
-            channels[message.channel.id].constructor === Object)
-            delete channels[message.channel.id];
-        message.channel.send("It's gone!");
-        saveChannels();
-    }
+        tClient.get('users/lookup', {screen_name : screenName})
+            .then(function(data) {
+                var userId = data[0].id_str;
+                if (!users.hasOwnProperty(userId))
+                    return message.channel.send("You're not currently `get`ting this user. Use `" + config.prefix + "startget "+ screenName +"` to do it!");
+                var idx = -1;
+                for (var i = 0 ; i < users[userId].channels.length ; i++)
+                {
+                    var channel = users[userId].channels[i].channel;
+                    if (channel.id == message.channel.id)
+                    {
+                        idx = i;
+                    }
+                }
+                if (idx == -1)
+                    message.channel.send("You're not currently `get`ting this user. Use `" + config.prefix + "startget "+ screenName +"` to do it!");
+                else
+                {
+                    // Remove element from channels
+                    users[userId].channels.splice(idx);
+                    // If channels is empty, we can delete the entry
+                    if (users[userId].channels.length == 0) {
+                        delete users[userId];
+                        // ...and re-register the stream
+                        createStream();
+                    }
 
-    if (command === "list")
-    {
-        if (!(message.channel.id in channels))
-            return message.channel.send("I'm afraid you don't have any oingoing `get`s! Use " + config.prefix + "startget to do that");
-        var str = "We are currently getting the users:\n";
-        for (var key in channels[message.channel.id])
-        {
+                   message.channel.send("It's gone!");
+                    saveUsers();
+                    return;
 
-            // ignore prototype properties
-            if (!channels[message.channel.id].hasOwnProperty(key))
-                continue;
-            str += key + " (Every " + channels[message.channel.id][key].delay + " minutes)\n";
-        }
-        message.channel.send(str);
+                }
+            })
+            .catch(function(err) {
+                return message.channel.send("I can't find a user by the name of " + screenName);
+            });
     }
 });
 
 dClient.on('ready', () => {
-    fs.stat(config.getFile, function(err, stat) {
-        if (err == null) {
-            console.log("Found a get file at " + config.getFile);
-            fs.readFile(config.getFile, 'utf8', function(err, data) {
-                if (err) {
-                    console.error("There was a problem reading the config file");
-                    return;
-                }
-                // Restore the channels object from saved file
-                var channelsCopy = JSON.parse(data);
-                // Re-create the thing at random intervals
-                console.log(channelsCopy);
-                for (var id in channelsCopy) { // Iterate over channels
-                    if (!channelsCopy.hasOwnProperty(id)) continue;
-
-                    var channel = channelsCopy[id];
-                    for (var name in channel) { // Iterate over gets in channels
-                        if (!channel.hasOwnProperty(name)) continue;
-
-                        // Need a closure for this to save the loop variables' state
-                        (function(channelId, screenName) {
-                            // Acquire the get object
-                            var get = channelsCopy[channelId][screenName];
-                            // Get the channel object from channel id
-                            var channel = dClient.channels.get(channelId);
-                            // Immediately add the interval
-                            addGet(channel, screenName, get.delay);
-                            // Queue up a request immediately, on a random timer to avoid overloading APIs
-                            setTimeout(function() {
-                                getLatestPic(channel, screenName);
-                            }, Math.floor((Math.random() * get.delay) * 1000 * 60));
-                        })(id, name);
-                    }
-                }
-            });
-        }
-    });
+    loadUsers();
 });
 
 dClient.login(pw.dToken);
