a0422a83aa33d443758b5d4a7392e659766657da
Élise <atomheartother@gmail.com>
2018-04-16 12:42:55 +0100

    Removed useless files

--- userList.js
+++
@@ -1,8 +0,0 @@
-// Users:
-// Dict of TwitterUser, using userId as key
-//  TwitterUser:
-//   name: screen name
-//   channels: Array of Gets
-//   Get:
-//    channel: channel object
-module.exports = {};
