e40ec29b242f4a9330e638f85e600d5839d59c3b
atomheartother <atomheartother@gmail.com>
2019-05-03 01:11:17 +0200

    Instantly regretted this, usage should be simple

--- usage.js
+++ usage.js
@@ -3,13 +3,13 @@ var config = require("./config.json");
 module.exports = {
   tweet: `Get the latest tweet from the given user and post it.\nUsage: \`${
     config.prefix
-  }tweet <twitter screen name|twitter url>\``,
+  }tweet <twitter screen name>\``,
   start: `Post a twitter user's tweets in real time.\nUsage: \`${
     config.prefix
-  }start <twitter screen name|twitter url> [--notext]\``,
+  }start <twitter screen name> [--notext]\``,
   stop: `Stop automatically posting tweets from the given user.\nUsage: \`${
     config.prefix
-  }stop <twitter screen name|twitter url>\``,
+  }stop <twitter screen name>\``,
   stopchannel: `Exactly like stop but acts on the whole channel.\nUsage: \`${
     config.prefix
   }stopchannel [channel ID]\``,
