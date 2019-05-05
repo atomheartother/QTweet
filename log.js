d725c48d94bca5caf681aac5cd040cd9837b1ca3
Élise <atomheartother@gmail.com>
2019-05-05 11:31:57 +0200

    Removed extra space in logs

--- log.js
+++ log.js
@@ -1,7 +1,7 @@
 module.exports = (message, channel = null) => {
   const dateString = new Date().toLocaleString();
   let channelInfo = channel
-    ? `[#${channel.name} -- ${channel.guild.name}]`
+    ? ` [#${channel.name} -- ${channel.guild.name}]`
     : "";
-  console.log(`${dateString}: ${channelInfo}`, message);
+  console.log(`${dateString}:${channelInfo}`, message);
 };
