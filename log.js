ff22a727935c3d85712b50a46b3b12fe7350f9ae
Élise <atomheartother@gmail.com>
2019-05-16 14:19:40 +0200

    Proper formatting for DM logs

--- log.js
+++ log.js
@@ -1,7 +1,10 @@
+const formatChannelInfo = channel =>
+  channel.type !== "dm"
+    ? ` [#${channel.name} -- ${channel.guild.name}]`
+    : ` [${channel.recipient.username}#${channel.recipient.discriminator}]`;
+
 module.exports = (message, channel = null) => {
   const dateString = new Date().toLocaleString();
-  let channelInfo = channel
-    ? ` [#${channel.name} -- ${channel.guild.name}]`
-    : "";
+  let channelInfo = channel ? formatChannelInfo(channel) : "";
   console.log(`${dateString}:${channelInfo}`, message);
 };
