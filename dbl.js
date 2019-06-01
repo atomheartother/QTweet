dda6d0c25d35e3020e7e63814cbe4dca47fc3f94
Élise <atomheartother@gmail.com>
2019-06-01 15:37:37 +0200

    Added DBL stats sending

---
+++ dbl.js
@@ -0,0 +1,21 @@
+const DBL = require("dblapi.js");
+let dblClient = null;
+const discord = require("./discord");
+const pw = require("./pw.json");
+const log = require("./log");
+
+let dbl = (module.exports = {});
+
+dbl.registerClient = () => {
+  if (pw.DBLToken === null) {
+    return;
+  }
+  dblClient = new DBL(pw.DBLToken, discord.getClient())
+    .on("posted", () => {
+      log("Sent our client stats to DBL");
+    })
+    .on("error", err => {
+      log("Error registering DBL client:");
+      log(err);
+    });
+};
