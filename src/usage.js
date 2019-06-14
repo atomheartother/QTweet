60b2a5694c4b925ff5ceffb5bbaf741a43404217
Élise <atomheartother@gmail.com>
2019-06-14 18:57:48 +0200

    Changed usage wording

--- src/usage.js
+++ src/usage.js
@@ -4,15 +4,15 @@ module.exports = {
   tweet: `Post the latest tweet from the given user.\n**Usage**: \`${
     config.prefix
   }tweet <twitter screen name>\``,
-  start: `Post a twitter user's tweets in real time.\n**Usage**: \`${
+  start: `Subscribe to a twitter user, getting their tweets in real time.\n**Usage**: \`${
     config.prefix
   }start <twitter screen name> [--notext]\`\nI can take up to 20 minutes to start posting their tweets.\nYou can add multiple twitter users by separating their screen names with spaces.`,
-  stop: `Stop automatically posting tweets from the given user.\nUsage: \`${
+  stop: `Unsubscribe from the given user.\nUsage: \`${
     config.prefix
   }stop <twitter screen name>\``,
   stopchannel: `Exactly like stop but acts on the whole channel.\n**Usage**: \`${
     config.prefix
   }stopchannel [channel ID]\``,
   list:
-    "Print a list of the twitter users you're currently fetching tweets from."
+    "Print a list of the twitter users you're currently subscribed to."
 };
