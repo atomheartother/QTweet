6c5d802d22f3732a80aff72457f2f7f142a3ea17
Élise <atomheartother@gmail.com>
2019-06-17 13:35:38 +0200

    Changed time format to 24h

--- src/log.js
+++ src/log.js
@@ -1,5 +1,5 @@
 module.exports = (message, qChannel = null) => {
-  const dateString = new Date().toLocaleString();
+  const dateString = new Date().toLocaleString("en-GB");
   let channelInfo = qChannel ? `[${qChannel.formattedName}]` : "";
   console.log(`${dateString}:${channelInfo}`, message);
 };
