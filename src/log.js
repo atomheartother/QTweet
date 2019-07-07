30687f8d0155f98ab845e6b88cdc70f8ecc463b7
Élise <atomheartother@gmail.com>
2019-07-07 10:02:15 +0200

    Fixed log bug

--- src/log.js
+++ src/log.js
@@ -1,8 +1,12 @@
 export default async (message, qChannel = null) => {
   const dateString = new Date().toLocaleString("en-GB");
-  const obj = await qChannel.obj();
-  let channelInfo = qChannel
-    ? `[${obj ? await qChannel.name() : `${qChannel.id} - ${qChannel.isDM}`}]`
-    : "";
-  console.log(`${dateString}:${channelInfo}`, message);
+  if (qChannel) {
+    const obj = await qChannel.obj();
+    const channelInfo = `[${
+      obj ? await qChannel.name() : `${qChannel.id} - ${qChannel.isDM}`
+    }]`;
+    console.log(`${dateString}:${channelInfo}`, message);
+  } else {
+    console.log(dateString, message);
+  }
 };
