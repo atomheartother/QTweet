8672a1803aa561e54940287f5cfbd7fa96550a32
Élise <atomheartother@gmail.com>
2019-07-07 15:38:40 +0200

    Added a sanity check to db launch

--- src/subs.js
+++ src/subs.js
@@ -11,11 +11,17 @@ import {
   addSubscription,
   removeSubscription,
   hasUser,
-  addUser,
+  getUserInfo as SQL_getUserInfo,
+  hasChannel,
+  addChannel,
+  getAllSubs,
+  addUser as SQL_addUser,
   open as openDb,
   close as closeDb,
   getGuildChannels
 } from "./sqlite";
+import log from "./log";
+import QChannel from "./QChannel";
 
 export const init = openDb;
 
@@ -39,10 +45,76 @@ export const getChannelSubs = SQL_getChannelSubs;
 
 export const getUserFromScreenName = SQL_getUserFromScreenName;
 
+export const addUser = SQL_addUser;
+
 export const addUserIfNoExists = async (twitterId, name) => {
   const shouldAddUser = !(await hasUser(twitterId));
   if (shouldAddUser) {
-    await addUser(twitterId, name);
+    const users = await addUser(twitterId, name);
+    return users;
+  }
+  return 0;
+};
+
+// Makes sure everything is consistent
+export const sanityCheck = async () => {
+  const allSubscriptions = await getAllSubs();
+  log(`Starting sanity check on ${allSubscriptions.length} subscriptions`);
+  for (let i = 0; i < allSubscriptions.length; i++) {
+    const sub = allSubscriptions[i];
+    const qc = QChannel.unserialize(sub);
+    const obj = await qc.obj();
+    if (!obj) {
+      const { subs, users } = await rmChannel(qc.id);
+      log(
+        `Found invalid qChannel: ${qc.id} (${
+          qc.isDM
+        }). Deleted ${subs} subs, ${users} users.`
+      );
+      continue;
+    }
+    const c = await addChannelIfNoExists(sub.channelId, sub.isDM);
+    if (c > 0) {
+      log(`Channel wasn't in channels table: ${sub.channelId}`);
+    }
+    const u = await addUserIfNoExists(sub.twitterId, "temp");
+    if (u > 0) {
+      log(`User ${sub.twitterId} wasn't in users table.`);
+    }
+  }
+  log(`Sanity check completed.`);
+};
+
+export const getUserInfo = SQL_getUserInfo;
+
+export const updateUser = async user => {
+  const usrInfo = await getUserInfo(user.id_str);
+  if (!usrInfo || usrInfo.name !== user.screen_name) {
+    addUser(user.id_str, user.screen_name);
+  }
+};
+
+export const addChannelIfNoExists = async (channelId, isDM) => {
+  const shouldCreateChannel = !(await hasChannel(channelId));
+  if (shouldCreateChannel) {
+    const qc = QChannel.unserialize({ channelId, isDM });
+    const obj = await qc.obj();
+    if (!obj) {
+      log(
+        `Somehow got a bad qChannel on a new subscription: ${channelId}, ${isDM}`
+      );
+      return 0;
+    }
+    if (qc.isDM) {
+      await addChannel(channelId, channelId, channelId, qc.isDM);
+    } else {
+      await addChannel(
+        channelId,
+        await qc.guildId(),
+        await qc.ownerId(),
+        qc.isDM
+      );
+    }
     return 1;
   }
   return 0;
@@ -53,7 +125,8 @@ export const add = async (channelId, twitterId, name, flags, isDM) => {
   const subs = await addSubscription(channelId, twitterId, flags, isDM);
   // If we didn't update any subs we don't have to check for new users
   const users = subs === 0 ? 0 : await addUserIfNoExists(twitterId, name);
-  return { subs, users };
+  const channels = subs === 0 ? 0 : await addChannelIfNoExists(channelId, isDM);
+  return { subs, users, channels };
 };
 
 export const rmUser = SQL_rmUser;
@@ -67,12 +140,22 @@ const deleteUserIfEmpty = async twitterId => {
   return 0;
 };
 
+const deleteChannelIfEmpty = async channelId => {
+  const subs = await getChannelSubs(channelId);
+  if (subs.length === 0) {
+    await rmChannel(channelId);
+    return 1;
+  }
+  return 0;
+};
+
 // Remove a subscription
 // If this user doesn't have any more subs, delete it as well
 export const rm = async (channelId, twitterId) => {
   const subs = await removeSubscription(channelId, twitterId);
   const users = subs === 0 ? 0 : await deleteUserIfEmpty(twitterId);
-  return { subs, users };
+  const channels = subs === 0 ? 0 : await deleteChannelIfEmpty(channelId);
+  return { subs, users, channels };
 };
 
 export const rmChannel = async channelId => {
