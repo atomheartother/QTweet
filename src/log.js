82908abe346e743f056760ff782869394ae2ab4d
atomheartother <atomheartother@gmail.com>
2019-07-07 09:38:34 +0200

    Trying to handle bad qchannel values better when logging

--- src/log.js
+++ src/log.js
@@ -1,5 +1,8 @@
 export default async (message, qChannel = null) => {
   const dateString = new Date().toLocaleString("en-GB");
-  let channelInfo = qChannel ? `[${await qChannel.formattedName()}]` : "";
+  const obj = await qChannel.obj();
+  let channelInfo = qChannel
+    ? `[${obj ? await qChannel.name() : `${qChannel.id} - ${qChannel.isDM}`}]`
+    : "";
   console.log(`${dateString}:${channelInfo}`, message);
 };
