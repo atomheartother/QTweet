e535ac62416c5b4e17e3a72170339d3a31f7b597
atomheartother <atomheartother@gmail.com>
2019-11-17 18:44:38 +0100

    Moved new algo logic to twitterFetch

---
+++ src/twitterFetch.js
@@ -0,0 +1,123 @@
+// This module takes care of using fetches to act as a stream
+import {postTweet, isValid} from './twitter';
+import { getAllUsers, updateRecommendedFetchDate, updateUserData} from './subs'
+
+// Check every 2 min
+const checkInterval = 1000 * 60 * 2;
+// Reset every 15min
+const resetInterval = 1000 * 60 * 15;
+// Add at least this many ms in case of failure
+const minDelay = 1000 * 60 * 15;
+
+// Max timeline requests per 15min window
+const maxRequests = 1000;
+// Total
+let requests = 0;
+let nextReset = Date.now();
+
+const userTimeline = params => {
+  requests++;
+  return tClient.get("statuses/user_timeline", params);
+};
+
+const getNextFetchDateFromTweets = (tweets, lastFetchDate) => {
+    const lastTweetDate = new Date(tweets[0].created_at);
+    if (!isNaN(lastTweetDate.getTime())) {
+        // First tweet is valid
+        if (tweets.length > 1) {
+            const secondTweetDate = new Date(tweets[1].created_at);
+            if (!isNaN(secondTweetDate.getTime())) {
+                // Ideal case: We have two tweets, we measure the time between them and assume the next post will happen with about the same frequency
+                return lastTweetDate.getTime() +  (lastTweetDate.getTime() - secondTweetDate.getTime()) * 1.1;
+            }
+        }
+        // We only have one valid tweet date.
+        if (lastFetchDate) {
+            // We have a lastFetchDate, giving us a second date to use as basis
+            return lastFetchDate.getTime() + (lastTweetDate.getTime() - lastFetchDate) * 1.1;
+        }
+        // We don't have a lastFetchDate. Use the current time and the tweet date to extrapolate the next tweet.
+        return Date.now() + (Date.now() - lastTweetDate.getTime()) * 2
+    }
+    // No valid date given for tweet. Use min delay
+    return minDelay;
+}
+
+// twitterId: The user's unique ID
+// lastFetchDate: The time we last got the user's data. Also the time !!start was run if we never got any tweets.
+//      Users are queued in order of last fetch date, so the ones who never get posted are always checked first.
+//      We call this queue privilege and you lose it when you tweet anything.
+// recommendedFetchDate: The date at which we think we should try again. Also the time !!start was run if the user was never checked.
+// lastTweetId: Last time we got a tweet, we recorded its last ID.
+const checkUser = async ({
+  twitterId,
+  lastFetchDate = Date.now(),
+  recommendedFetchDate = Date.now(),
+  lastTweetId
+}) => {
+  if (recommendedFetchDate > Date.now()) return; // If it's not your time, it's not your time, do nothing
+  if (requests > maxRequests) {
+    console.log(`Check occurred too early, next check at next reset ${nextReset}`);
+    // Set the next check to be @ next reset, don't update any other data to keep queue privileges
+    updateRecommendedFetchDate(twitterId, nextReset);
+    return;
+  }
+  // We should now get their latest tweets.
+  const params = {
+    user_id: twitterId,
+    tweet_mode: "extended"
+  };
+  if (lastTweetId) {
+    params.since_id = lastTweetId;
+  } else {
+    params.count = 1;
+  }
+  // This costs us one call
+  let tweets = await userTimeline(params);
+  // Filter out invalid tweets
+  tweets = tweets.filter(tweet => isValid(tweet));
+  if (tweets.length > 0) {
+    // Post tweet here
+    console.log("Posting tweets!");
+    const subs = await getUserSubs(tweets[0].user.id_str);
+    for (let i=tweets.length - 1 ; i >= 0 ; i--) {
+        console.log(tweets[i]);
+        postTweetWithSubs(tweets[i], subs);
+    }
+    const latestTweetDate = new Date(tweets[0].created_at)
+    updateUserData(twitterId, isNaN(latestTweetDate.getTime()) ? Date.now() : latestTweetDate.getTime(), getNextFetchDateFromTweets(tweets, lastFetchDate), tweets[0].id_str);
+    return;
+  }
+  // No tweets, bad user made us waste a request >:c
+  // Increase the recommended fetch date.
+  let nextDelay = lastFetchDate ? (Date.now() - lastFetchDate) * 2 : minDelay;
+  if (nextDelay < minDelay) {
+    // Apply minimum delay because they were bad boys
+    nextDelay = minDelay;
+  }
+  console.log(`No new tweets, new delay is ${nextDelay}ms`)
+  updateRecommendedFetchDate(twitterId, Date.now() + nextDelay);
+};
+
+const checkAllUsers = async () => {
+  nextReset = Date.now() + minDelay;
+  // Queue the next check in 15min
+  setTimeout(checkAllUsers, minDelay);
+  // This function fetches all twitter users & their metadata
+  // Data should be ordered by last fetch ASCENDING so we start with the oldest ones
+  const users = await getAllUsers();
+  // Check each user
+  for (let i = 0; i < users.length; i++) {
+    checkUser(users[i]);
+  }
+} 
+
+const requestReset = () => {
+    nextReset = Date.now() + resetInterval;
+    requests = 0;
+}
+
+export const init = () => {
+    setInterval(checkAllUsers, checkInterval);
+    setInterval(requestReset, resetInterval);
+};
