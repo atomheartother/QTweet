bf632f715011fb3411b38f05a923b2bc6de2d793
atomheartother <atomheartother@gmail.com>
2020-01-15 00:16:51 +0100

    Removed init script from application. Time to get serious

--- src/postgres.js
+++ src/postgres.js
@@ -6,33 +6,3 @@ const pool = new Pool()
 //     console.log(err, res)
 //     pool.end()
 // });
-
-export const init = async () => {
-    const client = await pool.connect()
-    try {
-        await client.query('BEGIN')
-        await client.query(`CREATE TABLE IF NOT EXISTS twitterUsers (
-            twitterId   integer PRIMARY KEY,
-            name        text
-        )`);
-        await client.query(`CREATE TABLE IF NOT EXISTS subs (
-          twitterId     integer,
-          channelId     integer,
-          isDM          integer NOT NULL,
-          flags         INTEGER NOT NULL,
-          CONSTRAINT    sub_key PRIMARY KEY(twitterId, channelId)
-        )`);
-    
-        await client.query(`CREATE TABLE IF NOT EXISTS guilds (
-            guildId     integer PRIMARY KEY,
-            lang TEXT NOT NULL DEFAULT '${
-                config.defaultLang
-            }'
-        )`)
-    } catch (e) {
-      await client.query('ROLLBACK')
-      throw e
-    } finally {
-      client.release()
-    }
-}
