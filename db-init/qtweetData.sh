b11d6fbeb1aa14fd765df5904d8e8516baf4b069
Élise <atomheartother@gmail.com>
2020-01-18 16:06:35 +0100

    List users, double quote table names

--- db-init/qtweetData.sh
+++ db-init/qtweetData.sh
@@ -3,26 +3,27 @@ set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 CREATE TABLE twitterUsers (
-    twitterId integer PRIMARY KEY,
-    name text
+    "twitterId" BIGINT PRIMARY KEY,
+    "name" text
 );
+
 CREATE TABLE subs (
-    twitterId integer,
-    channelId integer,
-    isDM integer NOT NULL,
-    flags integer NOT NULL,
-    CONSTRAINT sub_key PRIMARY KEY(twitterId, channelId)
+    "twitterId" BIGINT,
+    "channelId" BIGINT,
+    "isDM" boolean NOT NULL,
+    "flags" integer NOT NULL,
+    CONSTRAINT sub_key PRIMARY KEY("twitterId", "channelId")
 );
 
 CREATE TABLE guilds (
-    guildId     integer PRIMARY KEY,
-    lang        text NOT NULL DEFAULT '${DEFAULT_LANG}'
+    "guildId"     BIGINT PRIMARY KEY,
+    "lang"        text NOT NULL DEFAULT '${DEFAULT_LANG}'
 );
 
 CREATE TABLE channels (
-    channelId   integer PRIMARY KEY,
-    ownerId     integer NOT NULL,
-    guildId     integer NOT NULL,
-    isDM        integer NOT NULL DEFAULT 0
+    "channelId"   BIGINT PRIMARY KEY,
+    "ownerId"     BIGINT NOT NULL,
+    "guildId"     BIGINT NOT NULL,
+    "isDM"        integer NOT NULL DEFAULT 0
 );
 EOSQL
--- src/postgres.js
+++ src/postgres.js
@@ -16,9 +16,9 @@ const getInt = (val, alias = val) => `${val}::text AS ${alias}`;
 
 // Get all subscriptions
 export const getAllSubs = async () => {
-  const { rows } = await pool.query(`SELECT ${getInt('subs.channelId', 'channelId')}, ${getInt(
-    'twitterId',
-  )}, subs.isDM AS isDM, flags FROM subs`);
+  const { rows } = await pool.query(`SELECT ${getInt('subs."channelId"', '"channelId"')}, ${getInt(
+    '"twitterId"',
+  )}, subs."isDM" AS isDM, "flags" FROM subs`);
   return rows;
 };
 
@@ -27,10 +27,10 @@ export const getAllSubs = async () => {
 export const addSubscription = async (channelId, twitterId, flags, isDM) => {
   const { rows: [{ case: inserted }] } = await pool.query(`
   INSERT INTO 
-    subs(channelId, twitterId, flags, isDM)
+    subs("channelId", "twitterId", "flags", "isDM")
   VALUES($1, $2, $3, $4)
     ON CONFLICT ON CONSTRAINT sub_key
-  DO UPDATE SET flags=$3
+  DO UPDATE SET "flags"=$3
   RETURNING case when xmax::text::int > 0 then 0 else 1 end`,
   [channelId, twitterId, flags, isDM]);
   return inserted;
@@ -41,12 +41,16 @@ export const removeSubscription = async () => 0;
 export const getSubscription = async () => 0;
 
 export const getGuildSubs = async (guildId) => {
-  const { rows } = await pool.query(`SELECT ${getInt('subs.channelId', 'channelId')}, ${getInt('subs.twitterId', 'twitterId')},
-    name,
-    subs.isDM AS isDM,
-    flags FROM subs
-    INNER JOIN channels ON channels.channelId = subs.channelId INNER JOIN twitterUsers ON subs.twitterId = twitterUsers.twitterId
-    WHERE guildId = $1`,
+  const { rows } = await pool.query(`
+  SELECT 
+    ${getInt('subs."channelId"', '"channelId"')},
+    ${getInt('subs."twitterId"', '"twitterId"')},
+    "name",
+    subs."isDM" AS "isDM",
+    "flags"
+  FROM subs
+  INNER JOIN channels ON channels."channelId" = subs."channelId" INNER JOIN twitterUsers ON subs."twitterId" = twitterUsers."twitterId"
+  WHERE "guildId" = $1`,
   [guildId]);
   return rows;
 };
@@ -54,16 +58,16 @@ export const getGuildSubs = async (guildId) => {
 export const getChannelSubs = async (channelId, withName = false) => {
   const res = await pool.query(withName
     ? `SELECT ${getInt(
-      'subs.twitterId',
-      'twitterId',
+      'subs."twitterId"',
+      '"twitterId"',
     )},
-    name,
-    flags
-    FROM subs INNER JOIN twitterUsers ON subs.twitterId = twitterUsers.twitterId
-    WHERE subs.channelId=$1`
+    "name",
+    "flags"
+    FROM subs INNER JOIN twitterUsers ON subs."twitterId" = twitterUsers."twitterId"
+    WHERE subs."channelId"=$1`
     : `SELECT ${getInt(
-      'twitterId',
-    )}, flags FROM subs WHERE subs.channelId=$1`,
+      '"twitterId"',
+    )}, "flags" FROM subs WHERE subs."channelId"=$1`,
   [channelId]);
   return res.rows;
 };
@@ -71,43 +75,49 @@ export const getChannelSubs = async (channelId, withName = false) => {
 export const getUserSubs = async (twitterId, withInfo = false) => {
   const { rows } = await pool.query(
     withInfo
-      ? `SELECT ${getInt('subs.channelId', 'channelId')}, flags, ${getInt(
-        'guildId',
+      ? `SELECT ${getInt('subs."channelId"', '"channelId"')}, "flags", ${getInt(
+        '"guildId"',
       )}, ${getInt(
-        'ownerId',
-      )}, subs.isDM AS isDM FROM subs INNER JOIN channels ON subs.channelId = channels.channelId WHERE subs.twitterId=$1;`
+        '"ownerId"',
+      )}, "subs."isDM"" AS "isDM" FROM subs INNER JOIN channels ON subs."channelId" = channels."channelId" WHERE subs."twitterId"=$1;`
       : `SELECT ${getInt(
-        'channelId',
-      )}, flags, isDM FROM subs WHERE twitterId=$1`, [twitterId],
+        '"channelId"',
+      )}, "flags", "isDM" FROM subs WHERE "twitterId"=$1`, [twitterId],
   );
   return rows;
 };
 
 // Channel actions
 
-export const addChannel = async () => 0;
+export const addChannel = async (twitterId, name) => {
+  const { rowCount } = await pool.query('INSERT INTO twitterUsers("twitterId", "name") VALUES($1, $2) ON CONFLICT DO NOTHING', [twitterId, name]);
+  return rowCount;
+};
 
 export const rmChannel = async () => 0;
 
 export const getGuildChannels = async () => 0;
 
 export const getUniqueChannels = async () => {
-  const { rows } = await pool.query(`SELECT ${getInt('channelId')}, isDM FROM channels GROUP BY guildId`);
+  const { rows } = await pool.query(`SELECT ${getInt('"channelId"')}, "isDM" FROM channels GROUP BY "guildId"`);
   return rows;
 };
 
 // Twitter user actions
 export const getUserIds = async () => {
-  const { rows } = await pool.query(`SELECT ${getInt('twitterId')} FROM twitterUsers`);
+  const { rows } = await pool.query(`SELECT ${getInt('"twitterId"')} FROM twitterUsers`);
   return rows;
 };
 
 export const getUserInfo = async (twitterId) => {
-  const { rows } = await pool.query('SELECT name FROM twitterUsers WHERE twitterId = $1', [twitterId]);
+  const { rows } = await pool.query('SELECT "name" FROM twitterUsers WHERE "twitterId" = $1', [twitterId]);
   return rows;
 };
 
-export const addUser = async () => 0;
+export const addUser = async (twitterId, name) => {
+  const { rowCount } = await pool.query('INSERT INTO twitterUsers("twitterId", "name") VALUES($1, $2) ON CONFLICT DO NOTHING', [twitterId, name]);
+  return rowCount;
+};
 
 export const getUserFromScreenName = async () => 0;
 
