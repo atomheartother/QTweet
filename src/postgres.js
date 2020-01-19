af9823ad09bc65756d7e219957ec384377aef43a
Élise <atomheartother@gmail.com>
2020-01-19 20:21:13 +0100

    Fixed various postgres functions

--- src/postgres.js
+++ src/postgres.js
@@ -36,9 +36,22 @@ export const addSubscription = async (channelId, twitterId, flags, isDM) => {
   return inserted;
 };
 
-export const removeSubscription = async () => 0;
+export const removeSubscription = async (channelId, twitterId) => {
+  const { count } = await pool.query('DELETE FROM subs WHERE "channelId" = $1 AND "twitterId" = $2', [channelId, twitterId]);
+  return count;
+};
 
-export const getSubscription = async () => 0;
+export const getSubscription = async (channelId, twitterId, withName = false) => {
+  const { rows } = await pool.query(withName
+    ? `SELECT ${getInt('subs."channelId"', '"channelId"')}, ${getInt(
+      'subs."twitterId"',
+      '"twitterId"',
+    )}, "name", "flags", "isDM" FROM subs INNER JOIN twitterUsers ON subs."twitterId" = twitterUsers."twitterId" WHERE subs."channelId" = $1 AND subs."twitterId" = $2`
+    : `SELECT ${getInt('"channelId"')}, ${getInt(
+      '"twitterId"',
+    )}, "flags", "isDM" FROM subs WHERE "channelId" = $1 AND "twitterId" = $2`);
+  return rows[0];
+};
 
 export const getGuildSubs = async (guildId) => {
   const { rows } = await pool.query(`
@@ -89,14 +102,21 @@ export const getUserSubs = async (twitterId, withInfo = false) => {
 
 // Channel actions
 
-export const addChannel = async (twitterId, name) => {
-  const { rowCount } = await pool.query('INSERT INTO twitterUsers("twitterId", "name") VALUES($1, $2) ON CONFLICT DO NOTHING', [twitterId, name]);
+export const addChannel = async (channelId, guildId, ownerId, isDM) => {
+  const { rowCount } = await pool.query('INSERT INTO channels("channelId", "guildId", "ownerId", "isDM") VALUES($1, $2, $3, $4) ON CONFLICT DO NOTHING',
+    [channelId, guildId, ownerId, isDM]);
   return rowCount;
 };
 
-export const rmChannel = async () => 0;
+export const rmChannel = async (channelId) => {
+  const { count } = pool.query('DELETE FROM channels WHERE "channelId" = $1', [channelId]);
+  return count;
+};
 
-export const getGuildChannels = async () => 0;
+export const getGuildChannels = async (guildId) => {
+  const { rows } = pool.query(`SELECT ${getInt('"channelId"')} FROM channels WHERE "guildId" = $1`, [guildId]);
+  return rows;
+};
 
 export const getUniqueChannels = async () => {
   const { rows } = await pool.query(`SELECT ${getInt('"channelId"')}, "isDM" FROM channels GROUP BY "guildId"`);
@@ -119,11 +139,39 @@ export const addUser = async (twitterId, name) => {
   return rowCount;
 };
 
-export const getUserFromScreenName = async () => 0;
+export const getUserFromScreenName = async (name) => {
+  const { rows } = await pool.query(`SELECT ${getInt('"twitterId"')} FROM twitterUsers WHERE "name" = $1`, [name]);
+  return rows;
+};
 
-export const rmUser = async () => 0;
+export const rmUser = async (twitterId) => {
+  const { count } = await pool.query('DELETE FROM twitterUsers WHERE "twitterId" = $1', [twitterId]);
+  return count;
+};
 
-// Lang actions
-export const setLang = async () => 0;
+// Guild actions
+export const createGuild = async (guildId) => {
+  const { rowCount } = await pool.query('INSERT INTO guilds("guildId") VALUES($1) ON CONFLICT DO NOTHING',
+    [guildId]);
+  return rowCount;
+};
 
-export const getLang = async () => 0;
+export const rmGuild = async (guildId) => {
+  const { count } = await pool.query('DELETE FROM guilds WHERE "guildId" = $1', [guildId]);
+  return count;
+};
+
+export const setLang = async (guildId, lang) => {
+  const { rows: [{ case: inserted }] } = await pool.query(`INSERT INTO guilds("guildId", "lang")
+  VALUES($1, $2)
+  ON CONFLICT("guildId") DO
+    UPDATE SET "lang"=$2
+  RETURNING case when xmax::text::int > 0 then 0 else 1 end`,
+  [guildId, lang]);
+  return inserted;
+};
+
+export const getLang = async (guildId) => {
+  const { rows } = await pool.query('SELECT "lang" FROM guilds WHERE "guildId"=$1', [guildId]);
+  return rows[0];
+};
