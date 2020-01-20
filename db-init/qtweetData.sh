e0d42cf95d88a091cce2785e42fb710ca7883d92
atomheartother <atomheartother@gmail.com>
2020-01-20 23:13:26 +0100

    Added cascade deletions to db

--- db-init/qtweetData.sh
+++ db-init/qtweetData.sh
@@ -7,14 +7,6 @@ CREATE TABLE twitterUsers (
     "name" text
 );
 
-CREATE TABLE subs (
-    "twitterId" BIGINT,
-    "channelId" BIGINT,
-    "isDM" boolean NOT NULL,
-    "flags" integer NOT NULL,
-    CONSTRAINT sub_key PRIMARY KEY("twitterId", "channelId")
-);
-
 CREATE TABLE guilds (
     "guildId"     BIGINT PRIMARY KEY,
     "lang"        text NOT NULL DEFAULT '${DEFAULT_LANG}'
@@ -23,7 +15,16 @@ CREATE TABLE guilds (
 CREATE TABLE channels (
     "channelId"   BIGINT PRIMARY KEY,
     "ownerId"     BIGINT NOT NULL,
-    "guildId"     BIGINT NOT NULL,
+    "guildId"     BIGINT REFERENCES guilds ON DELETE CASCADE,
     "isDM"        boolean NOT NULL DEFAULT false
 );
+
+CREATE TABLE subs (
+    "twitterId" BIGINT REFERENCES twitterUsers ON DELETE CASCADE,
+    "channelId" BIGINT REFERENCES channels ON DELETE CASCADE,
+    "isDM" boolean NOT NULL,
+    "flags" integer NOT NULL,
+    CONSTRAINT sub_key PRIMARY KEY("twitterId", "channelId")
+);
+
 EOSQL
