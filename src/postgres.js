ec1657c913e4d8904549a41939a89a1b20442c94
atomheartother <atomheartother@gmail.com>
2020-01-24 23:22:09 +0100

    Fixed aggregation of channels in uniqueChannels

--- src/postgres.js
+++ src/postgres.js
@@ -157,7 +157,7 @@ export const getGuildChannels = async (guildId) => {
 };
 
 export const getUniqueChannels = async () => {
-  const { rows } = await pool.query(`SELECT ${getInt('"channelId"')}, "isDM" FROM channels GROUP BY "channelId", "guildId"`);
+  const { rows } = await pool.query(`SELECT ${getInt('MIN("channelId")', '"channelId"')}, BOOL_OR("isDM") FROM channels GROUP BY "guildId"`);
   return rows;
 };
 
