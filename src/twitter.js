2dd0097a017715ba59b80e76382cfa86c1535731
Thomas Navennec <atomheartother@gmail.com>
2020-02-23 18:28:53 +0100

    Changed API keys names for twitter

--- src/twitter.js
+++ src/twitter.js
@@ -21,10 +21,10 @@ const colors = Object.freeze({
 });
 
 const tClient = new Twitter({
-  consumer_key: process.env.TWITTER_ID,
-  consumer_secret: process.env.TWITTER_SECRET,
-  access_token_key: process.env.TWITTER_TOKEN,
-  access_token_secret: process.env.TWITTER_TOKEN_SECRET,
+  consumer_key: process.env.TWITTER_API_KEY,
+  consumer_secret: process.env.TWITTER_API_SECRET_KEY,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
 const reconnectionDelay = new Backup({
