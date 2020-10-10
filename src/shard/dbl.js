e6599c3a92082812f0b92af45613a86625c9bfa6
Élise <atomheartother@gmail.com>
2020-10-10 16:27:57 +0200

    Removed poster, the autoposter should do fine

--- src/shard/dbl.js
+++ src/shard/dbl.js
@@ -4,28 +4,6 @@ import log from '../log';
 
 let dblClient = null;
 
-const postDblStats = async () => {
-  const dClient = getClient();
-  if (!dClient.shard) {
-    log('❌ DBL: Tried to post stats but we don\'t have a shard object');
-    return;
-  }
-  if (dClient.shard.ids.length < 1) {
-    log('❌ DBL: 0 shards in dClient.shard.ids');
-    return;
-  }
-
-  if (dClient.shard.ids.length > 1) {
-    log('⚙️ DBL: We have more than 1 ID in this shard...');
-    log(dClient.shard.ids);
-  }
-  dblClient.postStats(
-    dClient.guilds.cache.array().length,
-    dClient.shard.ids[0],
-    dClient.shard.count,
-  );
-};
-
 export default () => {
   // Already initialized?
   if (dblClient) return;
@@ -37,6 +15,5 @@ export default () => {
     dblClient.on('error', ({ status }) => {
       log(`❌ DBL: Error with status ${status}`);
     });
-    setInterval(postDblStats, 1800000);
   }
 };
