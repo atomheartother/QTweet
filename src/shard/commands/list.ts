503fa880afb576379a34007e6bc6db420fb0ed9d
Élise <atomheartother@gmail.com>
2020-10-29 22:14:17 +0100

    Moved formatting functions to typescript

--- src/shard/commands/list.ts
+++ src/shard/commands/list.ts
@@ -1,18 +1,18 @@
 import { CmdFn } from ".";
 import { getChannelSubs, getLang } from "../../subs";
-import { formatSubsList } from "../format";
+import { formatSubsList, FORMAT_POST_EMBEDS, FORMAT_POST_TRANSLATED } from "../format";
 import { embeds, translated } from "../post";
 
 const list: CmdFn = async (_, qChannel) => {
     const gid = qChannel.guildId();
     const [subs, lan] = await Promise.all([getChannelSubs(qChannel.id, true), getLang(gid)]);
-    const { cmd: command, ...data } = await formatSubsList(qChannel.serialize(), subs, lan);
-    if (command === 'postList') {
-      const { embeds: pages } = data;
+    const fmtResult = await formatSubsList(qChannel.serialize(), subs, lan);
+    if (fmtResult.cmd === FORMAT_POST_EMBEDS) {
+      const { embeds: pages } = fmtResult;
       embeds(qChannel, pages);
     } else {
-      const { trCode, ...options } = data;
-      translated(qChannel, trCode, options);
+      const { trCode } = fmtResult;
+      translated(qChannel, trCode);
     }
 };
 
