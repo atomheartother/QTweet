b7e3ef4b547c9910353bee5a1b3c0a1f1a5d9ab1
Élise <atomheartother@gmail.com>
2021-02-02 23:47:56 +0100

    try/catch a channel not existing and ignore the message

--- src/shard/handleMasterMsg.ts
+++ src/shard/handleMasterMsg.ts
@@ -6,12 +6,15 @@ import { handleStart as start } from './commands/start';
 import { handleStop as stop } from './commands/stop';
 import { post, translated } from './post';
 import QChannel from './QChannel/QChannel';
-import i18n from './i18n';
 
 const handlePost = async ({ qc, content, type }) => {
   // This command can be broadcast to all shards, we must check that it's valid
   const qChannel = QChannel.unserialize(qc);
-  if (!(await qChannel.obj())) return;
+  try {
+    if (!(await qChannel.obj())) return;
+  } catch(e) {
+    return
+  }
   if (type === 'translated') {
     const { trCode: code, ...params } = content;
     translated(qChannel, code, params);
